37516ac133969a601ea6f0df2232b13408be7cf0
rozhiar <rozyar.313175000@uhd.edu.iq>
2022-11-04 13:16:01 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+// Code your solution in this file
+
+function distanceFromHqInBlocks(block) {
+  if (block > 42) {
+    return block - 42;
+  } else {
+    return 42 - block;
+  }
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  if (start > destination) {
+    return (start - destination) * 264;
+  } else {
+    return (destination - start) * 264;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
